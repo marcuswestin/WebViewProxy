7aff9d27218474ddda2ef7ad21e705d1a1bc2ee0
Marcus Westin <marcus.westin@gmail.com>
2012-10-02 09:44:37 -0700

    Add an example proxy request handler for responding with an image from the app package

--- ExampleWebViewProxyApp/ExampleWebViewProxyApp/AppDelegate.m
+++ ExampleWebViewProxyApp/ExampleWebViewProxyApp/AppDelegate.m
@@ -1,9 +1,15 @@
 #import "AppDelegate.h"
+#import "WebViewProxy.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    [WebViewProxy handleRequestsWithHost:@"foo" handler:^(WebViewProxyResponse *response) {
+        UIImage* image = [UIImage imageNamed:@"ExampleImage.png"];
+        [response respondWithImage:image];
+    }];
 
     UIWebView* webView = [[UIWebView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
     NSString* webViewContentPath = [[NSBundle mainBundle] pathForResource:@"WebViewContent" ofType:@"html"];
