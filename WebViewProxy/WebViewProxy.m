b957d1715b06e520d1706a89ca64bd3ffbaf1f58
Marcus Westin <marcus.westin@gmail.com>
2012-10-01 18:41:47 -0700

    flesh out WebViewProxyURLProtocol with all the required NSURLProtocol methods and basic default behavior. Next up: glue together WebViewProxyURLProtocol with registered WebViewProxyRequestMatchers.

--- WebViewProxy/WebViewProxy.m
+++ WebViewProxy/WebViewProxy.m
@@ -71,12 +71,67 @@ static NSMutableArray* requestMatchers;
 /* The NSURLProtocol implementation that allows us to intercept requests. */
 @interface WebViewProxyURLProtocol : NSURLProtocol
 @property (strong,nonatomic) NSURLConnection* connection;
-@property (strong,nonatomic) NSURLRequest* request;
+@property (strong,nonatomic) NSMutableURLRequest* request;
++ (WebViewProxyRequestMatcher*)findRequestMatcher:(NSURL*)url;
 @end
 
 /* The actual implementation of our NSURLProtocol subclass */
 @implementation WebViewProxyURLProtocol
+@synthesize connection=_connection, request=_request;
 
-@synthesize connection, request;
++ (WebViewProxyRequestMatcher *)findRequestMatcher:(NSURL *)url {
+    for (WebViewProxyRequestMatcher* requestMatcher in requestMatchers) {
+        if ([requestMatcher.predicate evaluateWithObject:url]) {
+            return requestMatcher;
+        }
+    }
+    return nil;
+}
+
++ (BOOL)canInitWithRequest:(NSURLRequest *)request {
+    return ([self findRequestMatcher:request.URL] != nil);
+}
+
++ (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
+    return request;
+}
+
+- (id)initWithRequest:(NSURLRequest *)request cachedResponse:(NSCachedURLResponse *)cachedResponse client:(id<NSURLProtocolClient>)client {
+    // TODO How to handle cachedResponse?
+    self.request = [request mutableCopy];
+    return self;
+}
+
+- (void)startLoading {
+    self.connection = [[NSURLConnection alloc] initWithRequest:self.request delegate:self];
+    if (!self.connection) {
+        [self.client URLProtocol:self didFailWithError:nil];
+    }
+}
+
+- (void)stopLoading {
+    [self.connection cancel];
+    self.connection = nil;
+}
+
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    // Check for redirect and possibly call [self.client URLProtocol:(NSURLProtocol *) wasRedirectedToRequest:(NSURLRequest *) redirectResponse:(NSURLResponse *)]??
+    [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [self.client URLProtocol:self didLoadData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    [self.client URLProtocolDidFinishLoading:self];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    [self.client URLProtocol:self didFailWithError:error];
+    self.connection = nil;
+}
+
 
 @end
