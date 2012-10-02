2166e540910bfb67c0710e2663dd038003df3cc8
Marcus Westin <marcus.westin@gmail.com>
2012-10-01 23:19:37 -0700

    Refine API. We now have [WebViewProxy handleRequestsWithScheme/Host/PathPrefix/Matching handler:^(response)], and for the response there is respondWithImage/Text/HTML/JSON/Data, as well as a piping API pipeResponse/Data/End. All the response methods and the piping API are largelly implemented

--- WebViewProxy/WebViewProxy.h
+++ WebViewProxy/WebViewProxy.h
@@ -1,25 +1,37 @@
 #import <Foundation/Foundation.h>
 
-@interface WebViewProxyResponse : NSObject
-@property (strong,nonatomic) NSMutableURLRequest* request;
-@property (strong,nonatomic) NSURLResponse* response;
+@interface WebViewProxyResponse : NSObject {
+    @private NSURLProtocol* _protocol;
+    @private id<NSURLProtocolClient> _client;
+    @private NSURLResponse* _response;
+    @private NSMutableDictionary* _headers;
+}
+@property (strong,nonatomic,readonly) NSURLRequest* request;
 // Convenience API
-- (void) respondWithData:(NSData*)data encoding:(NSStringEncoding)encoding;
 - (void) respondWithImage:(UIImage*)image;
-- (void) respondWithString:(NSString*)string;
+- (void) respondWithImage:(UIImage*)image cachingAllowed:(BOOL)cachingAllowed;
+- (void) respondWithText:(NSString*)text;
+- (void) respondWithHTML:(NSString*)html;
 - (void) respondWithJSON:(NSDictionary*)jsonObject;
 // Core API
+- (void) respondWithError:(NSInteger)statusCode text:(NSString*)text;
 - (void) setHeader:(NSString*)headerName value:(NSString*)headerValue;
-- (void) sendData:(NSData*)data;
-- (void) end;
+- (void) respondWithData:(NSData*)data mimeType:(NSString*)mimeType;
+- (void) respondWithData:(NSData*)data mimeType:(NSString*)mimeType cachingAllowed:(BOOL)cachingAllowed;
+// Pipe data API
+- (void) pipeResponse:(NSURLResponse*)response cachingAllowed:(BOOL)cachingAllowed;
+- (void) pipeResponse:(NSURLResponse*)response;
+- (void) pipeData:(NSData*)data;
+- (void) pipeEnd;
+// Private methods
+- (id) _initWithProtocol:(NSURLProtocol*)protocol Request:(NSURLRequest*)request client:(id<NSURLProtocolClient>)client;
 @end
 
 /* Our block definitions */
-typedef void (^WebViewProxyHandler)(NSURLRequest* request, WebViewProxyResponse* response);
+typedef void (^WebViewProxyHandler)(WebViewProxyResponse* response);
 
 /* The actual WebViewProxy API itself */
 @interface WebViewProxy : NSObject
-+ (void) setup;
 + (void) handleRequestsWithScheme:(NSString*)scheme handler:(WebViewProxyHandler)handler;
 + (void) handleRequestsWithHost:(NSString*)host handler:(WebViewProxyHandler)handler;
 + (void) handleRequestsWithPathPrefix:(NSString*)pathPrefix handler:(WebViewProxyHandler)handler;
--- WebViewProxy/WebViewProxy.m
+++ WebViewProxy/WebViewProxy.m
@@ -1,5 +1,7 @@
 #include "WebViewProxy.h"
 
+static NSMutableArray* requestMatchers;
+
 /* A single path matcher */
 @interface WebViewProxyRequestMatcher : NSObject
 @property (strong,nonatomic) NSPredicate* predicate;
@@ -16,69 +18,94 @@
 }
 @end
 
-/* This is the actual WebViewProxy API */
-static NSMutableArray* requestMatchers;
-@implementation WebViewProxy
-+ (void)setup {
-    requestMatchers = [NSMutableArray array];
-}
-+ (void)handleRequestsWithScheme:(NSString *)scheme handler:(WebViewProxyHandler)handler {
-    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"scheme MATCHES '%@'", scheme];
-    [self handleRequestsMatching:predicate handler:handler];
-}
-+ (void)handleRequestsWithHost:(NSString *)host handler:(WebViewProxyHandler)handler {
-    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"host MATCHES '%@'", host];
-    [self handleRequestsMatching:predicate handler:handler];
-}
-+ (void)handleRequestsWithPathPrefix:(NSString *)pathPrefix handler:(WebViewProxyHandler)handler {
-    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"path MATCHES '^%@.*'", pathPrefix];
-    [self handleRequestsMatching:predicate handler:handler];
-}
-+ (void)handleRequestsMatching:(NSPredicate*)predicate handler:(WebViewProxyHandler)handler {
-    // Match on any property of NSURL, e.g. "scheme MATCHES 'http' AND host MATCHES 'www.google.com'"
-    [requestMatchers addObject:[WebViewProxyRequestMatcher matchWithPredicate:predicate handler:handler]];
-}
-@end
-
 /* This is the proxy response object, through which we send responses */
 @implementation WebViewProxyResponse
 @synthesize request=_request;
+- (id)initWithProtocol:(NSURLProtocol *)protocol request:(NSURLRequest *)request client:(id<NSURLProtocolClient>)client {
+    if (self = [super init]) {
+        _protocol = protocol;
+        _request = request;
+        _client = client;
+        _headers = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
 // Convenience API
 - (void)respondWithData:(NSData *)data encoding:(NSStringEncoding)encoding {
 
 }
 - (void)respondWithImage:(UIImage *)image {
-    
+    [self respondWithImage:image cachingAllowed:YES];
+}
+- (void)respondWithImage:(UIImage *)image cachingAllowed:(BOOL)cachingAllowed {
+    NSURL* url = _request.URL;
+    NSString* mimeType = nil;
+    NSData* data = nil;
+    if ([url.pathExtension isEqualToString:@"jpg"] || [url.pathExtension isEqualToString:@"jpeg"]) {
+        mimeType = @"image/jpg";
+        data = UIImageJPEGRepresentation(image, 1.0);
+    } else {
+        // Default to PNG
+        if (![url.pathExtension isEqualToString:@"png"]) {
+            NSLog(@"WARNING WebViewProxy respondWithImage called for request with unknown file type \".%@\". Defaulting to image/png", url.absoluteString);
+        }
+        mimeType = @"image/png";
+        data = UIImagePNGRepresentation(image);
+    }
+    [self respondWithData:data mimeType:mimeType cachingAllowed:cachingAllowed];
 }
 - (void)respondWithJSON:(NSDictionary *)jsonObject {
-    
+    NSData* data = [NSJSONSerialization dataWithJSONObject:jsonObject options:0 error:nil];
+    [self respondWithData:data mimeType:@"application/json" cachingAllowed:NO];
 }
-- (void)respondWithString:(NSString *)string {
-    
+- (void)respondWithText:(NSString *)text {
+    NSData* data = [text dataUsingEncoding:NSUTF8StringEncoding];
+    [self respondWithData:data mimeType:@"text/plain" cachingAllowed:NO];
+}
+- (void)respondWithHTML:(NSString *)html {
+    NSData* data = [html dataUsingEncoding:NSUTF8StringEncoding];
+    [self respondWithData:data mimeType:@"text/html" cachingAllowed:NO];
 }
 // Core API
 - (void)setHeader:(NSString *)headerName value:(NSString *)headerValue {
-    [self.request setValue:headerValue forHTTPHeaderField:headerName];
+    [_headers setValue:headerValue forKey:headerName];
 }
-- (void)sendData:(NSData *)data {
-    
+- (void)respondWithData:(NSData *)data mimeType:(NSString *)mimeType {
+    [self respondWithData:data mimeType:mimeType cachingAllowed:NO];
 }
-- (void)end {
-    
+- (void)respondWithData:(NSData *)data mimeType:(NSString *)mimeType cachingAllowed:(BOOL)cachingAllowed {
+    NSURLCacheStoragePolicy cachePolicy = cachingAllowed ? NSURLCacheStorageAllowed : NSURLCacheStorageNotAllowed;
+    [_headers setValue:mimeType forKey:@"Content-Type"];
+    [_headers setValue:[NSNumber numberWithInt:data.length] forKey:@"Content-Length"];
+    NSHTTPURLResponse* response = [[NSHTTPURLResponse alloc] initWithURL:_request.URL statusCode:200 HTTPVersion:@"HTTP/1.1" headerFields:_headers];
+    [_client URLProtocol:_protocol didReceiveResponse:response cacheStoragePolicy:cachePolicy];
+    [_client URLProtocol:_protocol didLoadData:data];
+    [_client URLProtocolDidFinishLoading:_protocol];
+}
+// Pipe API
+- (void)pipeResponse:(NSURLResponse *)response cachingAllowed:(BOOL)cachingAllowed {
+    NSURLCacheStoragePolicy cachePolicy = cachingAllowed ? NSURLCacheStorageAllowed : NSURLCacheStorageNotAllowed;
+    [_client URLProtocol:_protocol didReceiveResponse:response cacheStoragePolicy:cachePolicy];
+}
+- (void)pipeData:(NSData *)data {
+    [_client URLProtocol:_protocol didLoadData:data];
+}
+- (void)pipeEnd {
+    [_client URLProtocolDidFinishLoading:_protocol];
 }
 @end
 
 
 /* The NSURLProtocol implementation that allows us to intercept requests. */
 @interface WebViewProxyURLProtocol : NSURLProtocol
-@property (strong,nonatomic) NSURLConnection* connection;
-@property (strong,nonatomic) NSMutableURLRequest* request;
+@property (strong,nonatomic) WebViewProxyResponse* proxyResponse;
+@property (strong,nonatomic) WebViewProxyRequestMatcher* requestMatcher;
 + (WebViewProxyRequestMatcher*)findRequestMatcher:(NSURL*)url;
 @end
 
 /* The actual implementation of our NSURLProtocol subclass */
 @implementation WebViewProxyURLProtocol
-@synthesize connection=_connection, request=_request;
+@synthesize proxyResponse=_proxyResponse, requestMatcher=_requestMatcher;
 
 + (WebViewProxyRequestMatcher *)findRequestMatcher:(NSURL *)url {
     for (WebViewProxyRequestMatcher* requestMatcher in requestMatchers) {
@@ -99,40 +126,42 @@ static NSMutableArray* requestMatchers;
 
 - (id)initWithRequest:(NSURLRequest *)request cachedResponse:(NSCachedURLResponse *)cachedResponse client:(id<NSURLProtocolClient>)client {
     // TODO How to handle cachedResponse?
-    self.request = [request mutableCopy];
+    self.requestMatcher = [self.class findRequestMatcher:request.URL];
+    self.proxyResponse = [[WebViewProxyResponse alloc] initWithProtocol:self request:request client:client];
     return self;
 }
 
 - (void)startLoading {
-    self.connection = [[NSURLConnection alloc] initWithRequest:self.request delegate:self];
-    if (!self.connection) {
-        [self.client URLProtocol:self didFailWithError:nil];
-    }
+    self.requestMatcher.handler(self.proxyResponse);
 }
 
 - (void)stopLoading {
-    [self.connection cancel];
-    self.connection = nil;
 }
 
 
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-    // Check for redirect and possibly call [self.client URLProtocol:(NSURLProtocol *) wasRedirectedToRequest:(NSURLRequest *) redirectResponse:(NSURLResponse *)]??
-    [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+@end
+
+/* This is the actual WebViewProxy API */
+@implementation WebViewProxy
++ (void)handleRequestsWithScheme:(NSString *)scheme handler:(WebViewProxyHandler)handler {
+    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"scheme MATCHES[cd] %@", scheme];
+    [self handleRequestsMatching:predicate handler:handler];
 }
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    [self.client URLProtocol:self didLoadData:data];
++ (void)handleRequestsWithHost:(NSString *)host handler:(WebViewProxyHandler)handler {
+    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"host MATCHES[cd] %@", host];
+    [self handleRequestsMatching:predicate handler:handler];
 }
-
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-    [self.client URLProtocolDidFinishLoading:self];
++ (void)handleRequestsWithPathPrefix:(NSString *)pathPrefix handler:(WebViewProxyHandler)handler {
+    NSString* pathPrefixRegex = [NSString stringWithFormat:@"^%@.*", pathPrefix];
+    NSPredicate* predicate = [NSPredicate predicateWithFormat:@"path MATCHES[cd] %@", pathPrefixRegex];
+    [self handleRequestsMatching:predicate handler:handler];
 }
-
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-    [self.client URLProtocol:self didFailWithError:error];
-    self.connection = nil;
++ (void)handleRequestsMatching:(NSPredicate*)predicate handler:(WebViewProxyHandler)handler {
+    if (!requestMatchers) {
+        requestMatchers = [NSMutableArray array];
+        [NSURLProtocol registerClass:[WebViewProxyURLProtocol class]];
+    }
+    // Match on any property of NSURL, e.g. "scheme MATCHES 'http' AND host MATCHES 'www.google.com'"
+    [requestMatchers addObject:[WebViewProxyRequestMatcher matchWithPredicate:predicate handler:handler]];
 }
-
-
 @end
