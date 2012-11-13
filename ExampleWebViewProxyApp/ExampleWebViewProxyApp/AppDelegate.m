c3668d1e3077803b7a4a95484dca760f628c7eb4
Marcus Westin <marcus.westin@gmail.com>
2012-11-13 10:19:27 -0800

    request google images asyncronously in example app

--- ExampleWebViewProxyApp/ExampleWebViewProxyApp/AppDelegate.m
+++ ExampleWebViewProxyApp/ExampleWebViewProxyApp/AppDelegate.m
@@ -3,18 +3,27 @@
 
 @implementation AppDelegate
 
+static NSOperationQueue* queue;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    queue = [[NSOperationQueue alloc] init];
+    [queue setMaxConcurrentOperationCount:5];
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     [WebViewProxy handleRequestsWithHost:@"www.google.com" path:@"/images/srpr/logo3w.png" handler:^(NSURLRequest* req, WVPResponse *res) {
-        UIImage* image = [UIImage imageWithData:[NSData dataWithContentsOfURL:req.URL]];
-        [res respondWithImage:image];
+        [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:req.URL] queue:queue completionHandler:^(NSURLResponse *netRes, NSData *data, NSError *netErr) {
+            if (netErr || ((NSHTTPURLResponse*)netRes).statusCode >= 400) { return [res respondWithError:500 text:@":("]; }
+            [res respondWithData:data mimeType:@"image/png"];
+        }];
     }];
 
     [WebViewProxy handleRequestsWithHost:@"black_and_white.google.com" path:@"/images/srpr/logo3w.png" handler:^(NSURLRequest* req, WVPResponse *res) {
         NSURL* url = [NSURL URLWithString:[req.URL.absoluteString stringByReplacingOccurrencesOfString:@"black_and_white" withString:@"www"]];
-        UIImage* originalImage = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
-        [res respondWithImage:[self blackAndWhite:originalImage]];
+        [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:url] queue:queue completionHandler:^(NSURLResponse *netRes, NSData *netData, NSError *netErr) {
+            if (netErr || ((NSHTTPURLResponse*)netRes).statusCode >= 400) { return [res respondWithError:500 text:@":("]; }
+            UIImage* originalImage = [UIImage imageWithData:netData];
+            [res respondWithImage:[self blackAndWhite:originalImage]];
+        }];
     }];
 
     [WebViewProxy handleRequestsWithHost:@"intercept" path:@"/Galaxy.png" handler:^(NSURLRequest* req, WVPResponse *res) {
